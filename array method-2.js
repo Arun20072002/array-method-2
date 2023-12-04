3d6a8b0450e1975da8050e8d8397b538ba9ab79c
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-12-04 10:59:28 +0530

    Array-method-2

---
+++ array method-2.js
@@ -0,0 +1,8 @@
+document.write("2.Get the number of elements present in a given array"+"<br>")
+document.write("---------------------------------------------------"+"<br>")
+
+var a = [10, 20, 30, 40];
+for(let i=0;i<=a.length-1;i++){
+    
+}
+document.write(a.length+"<br>")
